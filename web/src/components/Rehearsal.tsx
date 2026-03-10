52e10829635837191f246aae667d4802797aeca0
ComBba <cent@naver.com>
2026-03-11 03:57:23 +0900

    fix: restore valid Next layout and icon imports

--- web/src/components/Rehearsal.tsx
+++ web/src/components/Rehearsal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { MicrophoneIcon, CloudUploadIcon } from '@heroicons/react/24/solid';
+import { MicrophoneIcon } from '@heroicons/react/24/solid';
 import clsx from 'clsx';
 
 interface RehearsalProps {
